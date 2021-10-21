687d3b9c9a8655a5cd0524bbfa9299802cd8677f
Petar Kresoja <petarkresoja5@gmail.com>
2021-10-21 09:54:32 +0200

    Implemented lombok

--- src/main/java/com/samifying/link/error/ErrorObject.java
+++ src/main/java/com/samifying/link/error/ErrorObject.java
@@ -1,38 +1,20 @@
 package com.samifying.link.error;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Setter
 public class ErrorObject {
 
     private String name;
     private String message;
     private String path;
 
-    public ErrorObject(Exception e, String path) {
+    public ErrorObject(@NotNull Exception e, String path) {
         this.name = e.getClass().getName();
         this.message = e.getMessage();
-        this.path = path;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
         this.path = path;
     }
 }
